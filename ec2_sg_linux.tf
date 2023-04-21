2aea8742e4e1408e9475f549756137545bfd0437
Diego Medeiros <diegoomedeiros@gmail.com>
2023-04-21 18:59:29 -0300

    Arquivo de configuração de Security Group

---
+++ ec2_sg_linux.tf
@@ -0,0 +1,30 @@
+# Security group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "ssh_http_allowed" {
+  vpc_id      = aws_vpc.lab-vpc.id
+  name        = "ssh_http_allowed"
+  description = "Allow SSH and HTTP inbound traffic and full outbound"
+
+  egress { //Saida liberada
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress { //Entrada liberada na porta 22 para toda a internet
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress { //Entrada liberada na porta 80 para toda internet
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    env = var.env
+  }
+}
