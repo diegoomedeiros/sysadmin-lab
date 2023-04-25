487790e0ea69ed7715807d169d7c7435a4dc5e66
Diego Medeiros <diegoomedeiros@gmail.com>
2023-04-24 23:51:27 -0300

    Instalacao do Netdada e config do Nginx

--- linux_config.sh
+++ linux_config.sh
@@ -3,7 +3,7 @@
 until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
   sleep 1
 done
-# install nginx git
+# Instala nginx git
 apt-get update
 apt-get -y install nginx git
 
@@ -11,9 +11,27 @@ apt-get -y install nginx git
 git clone https://github.com/GitJMSeguradora/sysops-teste.git
 cp ./sysops-teste/app/* /var/www/html/
 
-# make sure nginx is started
+#Cria uma entrada no nginx para gerar estatisticas
+cat << 'EOF' | tee /etc/nginx/sites-enabled/nginx_stub
+server {
+        listen 127.0.0.1:80;
+        server_name 127.0.0.1;
+
+         location /nginx_status {
+                stub_status;
+        }
+}
+EOF
+
+# Confirmar que servico esta inicializado
 service nginx start
+
+#Instalacao NetData
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --no-updates --stable-channel --disable-telemetry --non-interactive
+
 #echo "nameserver 10.1.1.20" >> /etc/resolv.conf
+#Incluir dns-server como Servidor DNS principal do Web-server 
 sudo sed -i '/nameserver 127.0.0.53/i \nameserver 10.1.1.20 \' /etc/resolv.conf
+
 
 reboot
